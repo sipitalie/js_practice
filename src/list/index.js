31c6c9499ae25442a0749dc971821012debed385
yourShadow <sipitalie@gmai.com>
2021-07-28 14:36:46 +0100

    for in and for

--- src/list/index.js
+++ src/list/index.js
@@ -3,25 +3,25 @@ const service = require('../service/api');
 async function main() {
     try {
         const result = await service.obterPessoa('a')
-        const name = []
+        const nameFor = []
+        const nameForIn = []
         //usando for
-        //console.time('for')
-        //for (let i = 0; i <= result.results.length - 1; i++) {
-        //    const pessoa = result.results[i]
-        //    name.push(pessoa.name)
-        //}
-        //console.timeEnd('for')
+        console.time('for')
+        for (let i = 0; i <= result.results.length - 1; i++) {
+            const pessoa = result.results[i]
+            nameFor.push(pessoa.name)
+        }
+        console.timeEnd('for')
 
         //usando for in
         console.time('for in')
         for (let i in result.results) {
             const pessoa = result.results[i]
-            name.push(pessoa.name)
+            nameForIn.push(pessoa.name)
         }
         console.timeEnd('for in')
 
-
-        console.log('nomes=>:', name)
+        console.log('nomes=>:', nameFor, nameForIn)
     } catch (error) {
         console.error('error', error)
     }
